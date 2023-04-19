2c3add972c5655cec8ae19efbc65494f1ebc33d6
Zach Miller <zmilla93@gmail.com>
2023-04-19 14:17:08 -0500

    working model camera controller

--- scripts/model_camera_controller.js
+++ scripts/model_camera_controller.js
@@ -1,37 +1,25 @@
 class ModelCameraController extends Component {
-    // Camera data
     rotation = 0;
     yaw = 0;
     distance = 5;
-    angle = vec3.fromValues(0, 0, 1);
+    sensitivity = 20;
+    yawMax = 50;
     quat = quat.create();
-    light;
-    static origin = vec3.create();
-    // Ranges
-    constructor() {
-        super();
-        this.light = PointLight.create();
-    }
     update = function () {
         let camera = Camera.main;
-        // camera.position = this.angle;
-
-        if (Input.mousePressed(0)) {
-            // console.log("click!");
-        }
-        let sensitivity = 100;
-        if (Input.dragX != 0) {
-            this.rotation += Input.dragX * DEG2RAD * sensitivity;
-            vec3.rotateY(this.angle, this.angle, [0, 0, 0], Input.dragX * DEG2RAD);
-            vec3.rotateX(this.angle, this.angle, [0, 0, 0], Input.dragY * DEG2RAD * 20);
-        }
-        this.yaw -= Input.dragY * DEG2RAD * sensitivity;
+        // Apply mouse drag
+        this.rotation -= Input.dragX * DEG2RAD * this.sensitivity;
+        this.yaw -= Input.dragY * DEG2RAD * this.sensitivity;
+        // Clamp yaw
+        if (this.yaw > this.yawMax) this.yaw = this.yawMax;
+        if (this.yaw < -this.yawMax) this.yaw = -this.yawMax;
+        // Calculate camera position
         quat.fromEuler(this.quat, this.yaw, this.rotation, 0);
-        let axis = vec3.create()
-        // quat.getAxisAngle(axis, this.quat);
-        let original = vec3.fromValues(0, 0, 5);
-        vec3.transformQuat(axis, original, this.quat);
-
-        this.light.position = axis;
+        let targetPos = vec3.create()
+        let original = vec3.fromValues(0, 1.5, this.distance);
+        vec3.transformQuat(targetPos, original, this.quat);
+        // Apply camera position & rotation
+        camera.position = targetPos;
+        camera.setRotation(this.yaw, this.rotation, 0);
     }
 }
