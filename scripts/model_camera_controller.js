23631a6b79131e2d3dc76f7988ad90faccb50de9
Zach Miller <zmilla93@gmail.com>
2023-04-19 15:06:06 -0500

    added camera height

--- scripts/model_camera_controller.js
+++ scripts/model_camera_controller.js
@@ -1,13 +1,19 @@
 class ModelCameraController extends Component {
+    // Rotation & Yaw
     rotation = 0;
     yaw = 0;
-    distance = 5;
     sensitivity = 20;
-    scrollSensitivity = 0.5;
-    maxYaw = 50;
-    minDistance = 1;
-    maxDistance = 10;
+    maxYaw = 60;
+    // Distance
+    distance = 2;
+    minDistance = 0.5;
+    maxDistance = 7;
+    scrollStep = 0.2;
+    // Height
     targetHeight = 1.5;
+    heightStep = 0.02;
+    minHeight = 0;
+    maxHeight = 2;
     // Internal
     quat = quat.create();
     targetPos = vec3.create();
@@ -19,9 +25,14 @@ class ModelCameraController extends Component {
         if (this.yaw > this.maxYaw) this.yaw = this.maxYaw;
         if (this.yaw < -this.maxYaw) this.yaw = -this.maxYaw;
         // Apply mouse scroll
-        this.distance += Input.scrollNormalized * this.scrollSensitivity;
+        this.distance += Input.scrollNormalized * this.scrollStep;
         if (this.distance < this.minDistance) this.distance = this.minDistance;
         if (this.distance > this.maxDistance) this.distance = this.maxDistance;
+        // Apply height movement
+        if (Input.isKeyPressed("Space")) this.targetHeight += this.heightStep;
+        if (Input.isKeyPressed("ShiftLeft") || Input.isKeyPressed("ShiftRight")) this.targetHeight -= this.heightStep;
+        if (this.targetHeight < this.minHeight) this.targetHeight = this.minHeight;
+        if (this.targetHeight > this.maxHeight) this.targetHeight = this.maxHeight;
         // Calculate camera position
         quat.fromEuler(this.quat, this.yaw, this.rotation, 0);
         let original = vec3.fromValues(0, 0, this.distance);
