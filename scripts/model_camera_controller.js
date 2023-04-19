92f87d08259a8e651d27d76d40f2804632c544d5
Zach Miller <zmilla93@gmail.com>
2023-04-19 14:32:35 -0500

    added zoom to model camera controller

--- scripts/model_camera_controller.js
+++ scripts/model_camera_controller.js
@@ -3,16 +3,23 @@ class ModelCameraController extends Component {
     yaw = 0;
     distance = 5;
     sensitivity = 20;
-    yawMax = 50;
+    scrollSensitivity = 0.5;
+    maxYaw = 50;
+    minDistance = 3;
+    maxDistance = 10;
+    // Internal
     quat = quat.create();
     update = function () {
         let camera = Camera.main;
         // Apply mouse drag
         this.rotation -= Input.dragX * DEG2RAD * this.sensitivity;
         this.yaw -= Input.dragY * DEG2RAD * this.sensitivity;
-        // Clamp yaw
         if (this.yaw > this.yawMax) this.yaw = this.yawMax;
         if (this.yaw < -this.yawMax) this.yaw = -this.yawMax;
+        // Apply mouse scroll
+        this.distance += Input.scrollNormalized * this.scrollSensitivity;
+        if (this.distance < this.minDistance) this.distance = this.minDistance;
+        if (this.distance > this.maxDistance) this.distance = this.maxDistance;
         // Calculate camera position
         quat.fromEuler(this.quat, this.yaw, this.rotation, 0);
         let targetPos = vec3.create()
