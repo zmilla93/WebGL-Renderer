686f3f33f7ac99f8eb7c136114bfce03b38973a9
Zach Miller <zmilla93@gmail.com>
2023-04-19 14:48:28 -0500

    camera controller fixes

--- scripts/model_camera_controller.js
+++ scripts/model_camera_controller.js
@@ -5,28 +5,28 @@ class ModelCameraController extends Component {
     sensitivity = 20;
     scrollSensitivity = 0.5;
     maxYaw = 50;
-    minDistance = 3;
-    maxDistance = 10;
+    minDistance = 2;
+    maxDistance = 15;
     // Internal
     quat = quat.create();
+    targetPos = vec3.create();
     update = function () {
         let camera = Camera.main;
         // Apply mouse drag
         this.rotation -= Input.dragX * DEG2RAD * this.sensitivity;
         this.yaw -= Input.dragY * DEG2RAD * this.sensitivity;
-        if (this.yaw > this.yawMax) this.yaw = this.yawMax;
-        if (this.yaw < -this.yawMax) this.yaw = -this.yawMax;
+        if (this.yaw > this.maxYaw) this.yaw = this.maxYaw;
+        if (this.yaw < -this.maxYaw) this.yaw = -this.maxYaw;
         // Apply mouse scroll
         this.distance += Input.scrollNormalized * this.scrollSensitivity;
         if (this.distance < this.minDistance) this.distance = this.minDistance;
         if (this.distance > this.maxDistance) this.distance = this.maxDistance;
         // Calculate camera position
         quat.fromEuler(this.quat, this.yaw, this.rotation, 0);
-        let targetPos = vec3.create()
         let original = vec3.fromValues(0, 1.5, this.distance);
-        vec3.transformQuat(targetPos, original, this.quat);
+        vec3.transformQuat(this.targetPos, original, this.quat);
         // Apply camera position & rotation
-        camera.position = targetPos;
+        camera.position = this.targetPos;
         camera.setRotation(this.yaw, this.rotation, 0);
     }
 }
